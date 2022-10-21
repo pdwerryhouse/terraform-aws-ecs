c111b069bb578a9dde9cbc648507feb8b825bda5
Paul Dwerryhouse <paul@dwerryhouse.com.au>
2022-10-21 19:19:57 +1100

    add locals.tf

---
+++ locals.tf
@@ -0,0 +1,12 @@
+local {
+  user_data_ecs = <<EODATA
+
+cat >>/etc/ecs/ecs.config <<EOF
+ECS_CLUSTER="${var.name}-${var.env}-ecs-cluster"
+EOF
+
+service docker restart
+EODATA
+
+  user_data = join("\n". [var.user_data, local.user_data_ecs])
+}
